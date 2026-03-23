3252d82798374305156396792fde80153fdd833c
Guilherme <gtoninjorge@gmail.com>
2026-03-23 15:52:26 -0300

    Atividade: Sistema de Clínica com Médico e Paciente

--- src/atividades/ClinicaMedica/Main.java
+++ src/atividades/ClinicaMedica/Main.java
@@ -1,9 +1,20 @@
 package atividades.ClinicaMedica;
 
+import atividades.ClinicaMedica.modelo.Consulta;
+import atividades.ClinicaMedica.modelo.Medico;
+import atividades.ClinicaMedica.modelo.Paciente;
+
+import java.time.LocalDate;
+
 public class Main {
     static void main(String[] args) {
 
+        Medico m1 = new Medico("Roger" ,"Oftalmologista");
+        Paciente p1 = new Paciente("Miguel", "123.123.123-12");
 
+        LocalDate data = LocalDate.of(2026, 5, 20);
+        Consulta c1 = new Consulta(data, 200, m1, p1);
 
+        System.out.println(c1);
     }
 }
