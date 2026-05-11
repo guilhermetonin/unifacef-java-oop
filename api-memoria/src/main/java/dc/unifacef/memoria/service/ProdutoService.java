6f9ac394b76c332a82933ebf0b89aaecf651915a
Guilherme <gtoninjorge@gmail.com>
2026-05-11 10:25:04 -0300

    Adicionar projeto com API com estrutura Spring Boot

---
+++ api-memoria/src/main/java/dc/unifacef/memoria/service/ProdutoService.java
@@ -0,0 +1,27 @@
+package dc.unifacef.memoria.service;
+
+import dc.unifacef.memoria.model.Produto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class ProdutoService {
+    //  cria o arraylist
+    private List<Produto> produtos = new ArrayList<Produto>();
+    private Long id = 1L;
+
+    // consulta os produtos na lista
+    public List<Produto> consulta() {
+        return this.produtos;
+    }
+
+    public Produto cria(Produto produto) {
+        produto.setId(id);
+        id++;
+        this.produtos.add(produto);
+        return produto;
+    }
+}
